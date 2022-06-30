2fe92f3bb8323b13bc8122c12a74461a6671f71e
DESKTOP-NFT79S5\mine6 <mine6546@gmail.com>
2022-06-30 23:56:52 +0900

    About me 제작

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,11 @@
 export default function navbar() {
   return (
-    <div className="max-w-full">
-      <div className="m-auto w-2/3 bg-green-300">
+    <div className="relative">
+      <div className="fixed left-1/2 w-2/3 -translate-x-1/2 bg-green-300 py-5">
         <div className="flex">
-          <div className="w-2/3 bg-red-300">d</div>
+          <div className="w-2/3 bg-red-300 text-lg font-bold">
+            Kim's PortFolio
+          </div>
           <nav className="flex w-full justify-between font-bold">
             <li className="text-lg">Home</li>
             <li className="text-lg">About me</li>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/navbar";
-
+import desk from "../public/desk.jpg";
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <article>
+        <Image className="-z-10" src={desk} width={2000} height={600}></Image>
+      </article>
+      <article>
+        <div>About me</div>
+      </article>
     </div>
   );
 }
