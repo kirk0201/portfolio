e0328d129b09422fff2730687ef13731e36777a5
DESKTOP-NFT79S5\mine6 <mine6546@gmail.com>
2022-07-03 00:01:52 +0900

    Feat : 프로젝트 추가 중
    FLIXNET, Maiim 추가 예정

---
+++ components/Summary.js
@@ -0,0 +1,20 @@
+export default function Summary({ fn, content, url, urls }) {
+  return (
+    <div className="flex justify-between py-3">
+      <div className="w-1/3 font-semibold text-gray-400">{fn}</div>
+      <div>
+        <div className="font-semibold">{content}</div>
+        {url ? (
+          <a className="text-blue-500" href={url} target="_blank">
+            {url}
+          </a>
+        ) : undefined}
+        {urls ? (
+          <a className="block text-blue-500" href={urls} target="_blank">
+            {urls}
+          </a>
+        ) : undefined}
+      </div>
+    </div>
+  );
+}
